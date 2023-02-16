755818d1309f0bc69088694721a4723ff5aac0fd
Rushka <rushkaismail1@gmail.com>
2023-02-16 09:28:44 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * writing a script with printf
+ * must return 0
+ * no warnings when using -Wall gcc option
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
