ba4503ce682810a543295b541de37d89026141f9
Rushka <rushkaismail1@gmail.com>
2023-02-16 10:53:31 -0800

    6 for betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 
-int main(void) /* this is a comment on main */
+int main(void)
 {
+
+	/* this is a comment on main */
+
 	printf("Size of a char:%2d byte(s)\n", sizeof(char));
 	printf("Size of an int:%2d byte(s)\n", sizeof(int));
 	printf("Size of a long int:%2d byte(s)\n", sizeof(long int));
