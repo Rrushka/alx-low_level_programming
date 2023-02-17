388cf242a32f83dc737264481ca09fc72b237fdf
Rushka <rushkaismail1@gmail.com>
2023-02-17 06:11:40 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*task 4 - print lower case alphabet without q and e */
+
+/**
+ * main - start
+ * only use putchar twice
+ * Return: 0
+ */
+int main(void)
+{
+	char aTOz = 'a';
+
+	while (aTOz <= 'z' && aTOz != 'q' && aTOz != 'e')
+	{
+		putchar(aTOz);
+		aTOz++;
+	}
+	putchar('\n');
+	return (0);
+}
