9935a544a47e7ee5a7b7bfdc7c0ce45a264e409f
Rushka <rushkaismail1@gmail.com>
2023-02-17 07:53:05 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* task 6 -numbers */
+
+/**
+ * main - start
+ * putchar to print single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int numz;
+	for ( numz = 0; numz < 10; numz++)
+	{
+		putchar(numz);
+	}
+	putchar('\n');
+	return (0);
+}
+
