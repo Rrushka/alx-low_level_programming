9ad01fc37ffbe29ccc4d84769d327a92675244f1
Rushka <rushkaismail1@gmail.com>
2023-02-20 08:51:35 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* initial comment */
+
+/**
+ * main - entry point
+ * dont use printf
+ * Return: 1
+ */
+int main(void)
+{
+	dprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
