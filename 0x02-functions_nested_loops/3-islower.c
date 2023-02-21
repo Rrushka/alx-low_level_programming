971eb11f7e315a8334afa70396945c7ef4b63337
Rushka <rushkaismail1@gmail.com>
2023-02-21 10:20:16 -0800

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,9 @@
 
 /**
  * _islower - start
- * c - variable that the if statement is used to see if its upper of lower case
+ * int c - variable that the if statement is used to see if its upper of lower case
  * check for lower case character
- * Return: 1 if true
- * Return: 0 if not
+ * Return: 1 if true and 0 if not
  */
 int _islower(int c)
 {
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,8 @@
 /* task 4 */
 
 /**
- * _isalpha - function
- * c - check
+ * _isalpha - function0
+ * int c - check
  * Return: 1 if true
  * Return: 0 if not true
  */
@@ -12,9 +12,9 @@ int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
-	
+
 		return (1);
-	
+
 	}
 	else
 	{
