091e49160ad7a2c3244621416f2e8708d81b118a
Rushka <rushkaismail1@gmail.com>
2023-02-21 06:31:53 -0800

    alpha10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/* print alphabet x10 */
+
+/**
+ * print_alphabet_x10 - start
+ * only use _putchar twice
+ * no returns because void return type
+ */
+void print_alphabet_x10(void)
+{
+	char alpha10;
+	int n = 0;
+
+	while (n < 10)
+	{
+	while (alpha10 = 'a'; alpha10 <= 'z'; alpha10++)
+	{
+		_putchar(alpha10);
+	}
+		_putchar('\n');
+	}
+}
