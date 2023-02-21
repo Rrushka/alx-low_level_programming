586ec749c0e666ff69bb4114630288233ae80853
Rushka <rushkaismail1@gmail.com>
2023-02-21 02:04:53 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* print putchar */
+
+/**
+ * main - entry point
+ * print out _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
