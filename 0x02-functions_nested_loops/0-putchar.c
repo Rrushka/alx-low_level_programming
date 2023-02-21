884bb5518652555347bfbdfbcd2fc03cc6da952c
Rushka <rushkaismail1@gmail.com>
2023-02-21 04:30:56 -0800

    putchar only

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /* print putchar */
 
 /**
@@ -9,7 +8,15 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
