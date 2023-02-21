10c0efaefe49c2e8ba952da6267aea07ee242fdf
Rushka <rushkaismail1@gmail.com>
2023-02-21 05:17:48 -0800

    made main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* filename: main.h
+ * this is a header file that declares all the functions that i will use in 0x02-functions_nested_loops */
+
+int putchar(int char)
+
+#endif
