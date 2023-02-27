78792c1bc9134022261798f9452bf6caf3f5469f
Rushka <rushkaismail1@gmail.com>
2023-02-27 13:01:26 -0800

    fixed white space in 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,7 +5,7 @@
 /**
  * _strcpy - copies a string pointed to by * to dest
  * @src: string being referenced to
- * @dest: copy string to this 
+ * @dest: copy string to this
  * Return: pointer to @dest
  */
 char *_strcpy(char *dest, char *src)
