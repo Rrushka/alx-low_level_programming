4bc4e111c60e1bee584e041e673ef3ccec944d14
Rushka <rushkaismail1@gmail.com>
2023-03-06 11:26:27 -0800

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,7 @@
  * _strchr - locates a character in a string
  * @s: string
  * @c: character of interest
- * Return: pointer to character or NULL 
+ * Return: pointer to character or NULL
  */
 char *_strchr(char *s, char c)
 {
@@ -15,7 +15,7 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 	if (s[i] == c)
-		return(s[i]);
+		return(s);
 	}
 	return ('\0');
 }
